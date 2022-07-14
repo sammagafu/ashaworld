65e5c13ae5518441d6596f82a76417896a64a61f
Bichwaa <ptonny13@gmail.com>
2022-07-14 11:38:50 +0300

    added new package for a typeahead component

--- ashaadmin/src/main.js
+++ ashaadmin/src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import '../src/assets/css/asha.css'
+import SimpleTypeahead from 'vue3-simple-typeahead';
+import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css'; 
 // import 'flowbite';
 
 
 
 axios.defaults.baseURL = 'http://localhost:8000/api/v1/'
-createApp(App).use(store).use(router,axios).mount('#app')
+createApp(App).use(store).use(SimpleTypeahead).use(router,axios).mount('#app')
