090d1f1b5f4f73074b359cab21e7c822fb3fd5ea
bichwaa <ptonny13@gmail.com>
2022-07-18 00:43:47 +0300

    orders straightened

--- ashaadmin/src/views/Orders.vue
+++ ashaadmin/src/views/Orders.vue
@@ -136,7 +136,6 @@
                 axios.get('pos/order')
                     .then(response => {
                         this.order = response.data;
-                        console.log('order :>> ', response.data);
                     }).catch(error => {
                         console.log(error);
                     });
