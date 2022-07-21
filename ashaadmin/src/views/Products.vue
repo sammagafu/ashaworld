7ded448ca3b648e46aa21d2dce404ad4255767db
bichwaa <ptonny13@gmail.com>
2022-07-21 17:25:51 +0300

    procusts form tabs work

--- ashaadmin/src/views/Products.vue
+++ ashaadmin/src/views/Products.vue
@@ -179,35 +179,35 @@
           <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
             <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab"
               data-tabs-toggle="#myTabContent" role="tablist">
-              <li class="mr-2" role="presentation" @click="isActive == 'information'">
+              <li class="mr-2" role="presentation" @click="isActive = 'information'">
                 <button
                   class="inline-block p-4 rounded-t-lg border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500"
-                  id="profile-tab" data-tabs-target="#profile" type="button" role="tab" aria-controls="profile"
+                  id="information-tab" data-tabs-target="#information" type="button" role="tab" aria-controls="profile"
                   aria-selected="true">Information</button>
               </li>
-              <li class="mr-2" role="presentation" @click="isActive == 'images'">
+              <li class="mr-2" role="presentation" @click="isActive = 'images'">
                 <button
                   class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard"
+                  id="images-tab" data-tabs-target="#images" type="button" role="tab" aria-controls="dashboard"
                   aria-selected="false">Images</button>
               </li>
-              <li class="mr-2" role="presentation" @click="isActive == 'pricing'">
+              <li class="mr-2" role="presentation" @click="isActive = 'pricing'">
                 <button
                   class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings"
+                  id="pricing-tab" data-tabs-target="#pricing" type="button" role="tab" aria-controls="settings"
                   aria-selected="false">Pricing</button>
               </li>
-              <li role="presentation" @click="isActive == 'invetory' ">
+              <li role="presentation" @click="isActive = 'invetory' ">
                 <button
                   class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="contacts-tab" data-tabs-target="#contacts" type="button" role="tab" aria-controls="contacts"
+                  id="inventory-tab" data-tabs-target="#inventory" type="button" role="tab" aria-controls="contacts"
                   aria-selected="false">Invetory</button>
               </li>
             </ul>
           </div>
           <form class="space-y-6" action="#">
             <div id="myTabContent">
-              <div class="p-4" id="profile" role="tabpanel" aria-labelledby="profile-tab" :class="{ 'hidden': isActive != 'information' }">
+              <div class="p-4" id="information" role="tabpanel" aria-labelledby="information-tab" :class="{ 'hidden': isActive != 'information' }">
                 <div class="mb-6">
                   <label for="product" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product
                     name</label>
@@ -247,8 +247,8 @@
 
               <div
               :class="{ 'hidden': isActive != 'images' }"
-               class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="dashboard" role="tabpanel"
-                aria-labelledby="dashboard-tab">
+               class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="images" role="tabpanel"
+                aria-labelledby="images-tab">
                 <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
                     class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>.
                   Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps
@@ -256,8 +256,8 @@
               </div>
               <div 
               :class="{ 'hidden': isActive != 'pricing' }"
-              class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings" role="tabpanel"
-                aria-labelledby="settings-tab">
+              class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="pricing" role="tabpanel"
+                aria-labelledby="pricing-tab">
                 <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
                     class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>.
                   Clicking
@@ -266,8 +266,8 @@
               </div>
               <div 
               :class="{ 'hidden': isActive != 'invetory' }"
-              class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="contacts" role="tabpanel"
-                aria-labelledby="contacts-tab">
+              class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="inventory" role="tabpanel"
+                aria-labelledby="inventory-tab">
                 <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
                     class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>.
                   Clicking
