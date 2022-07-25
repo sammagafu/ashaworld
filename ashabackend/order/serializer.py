c5fe325f590c6f76dcde796f57427075ddf026c8
bichwaa <ptonny13@gmail.com>
2022-07-25 08:00:42 +0300

    orders have products"

--- ashabackend/order/serializer.py
+++ ashabackend/order/serializer.py
@@ -1,30 +1,28 @@
 from rest_framework import serializers
 from .models import Order,OrderItems
-from product.models import Product
 from product.serializer import ProductSerializer
 
 class OrderProductSerializer(serializers.ModelSerializer):
-    # product = ProductSerializer(read_only=True)
-    product = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    product = ProductSerializer(read_only=True)
     class Meta:
         model = OrderItems
         fields = ['product','quantity','order']
         read_only = ('id','created_at')
 
 class OrderSerializer(serializers.ModelSerializer):
-    # orderproducts = OrderProductSerializer(many=True)
-    orderproducts = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
-    # owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    orderproducts = OrderProductSerializer(many=True)
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
+
         model = Order
-        read_only = ('owner','paid_at','created_at', 'id')
-        fields = ['owner','totalprice','orderstatus', 'id','active','promo_code','orderproducts','slug','created_at']
-        depth = 1
+        read_only = ('owner','paid_at','created_at')
+        fields = ['owner','totalprice','orderstatus','active','promo_code','orderproducts','slug','created_at']
+        
     
     def create(self, validated_data):
         orderitems = validated_data.pop('orderproducts')
         print(orderitems)
         order = Order.objects.create(**validated_data)
-        for orderitem in orderitems:
-            OrderItems.objects.create(product=orderitem, order=order)
+        for orderitems in orderitems:
+            orderitems.objects.create(order=order, **orderitems)
         return order
