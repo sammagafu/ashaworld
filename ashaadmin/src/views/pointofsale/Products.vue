e761f8f1e3e883d7fc6c0854f6cdf9a9c1a1eee6
bichwaa <ptonny13@gmail.com>
2022-07-27 02:15:02 +0300

    POS products can be sorted

--- ashaadmin/src/views/pointofsale/Products.vue
+++ ashaadmin/src/views/pointofsale/Products.vue
@@ -70,10 +70,12 @@
               All Products
             </button>
             <button type="button"
+              @click="sortProductsByStatus"
               class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
               available
             </button>
             <button type="button"
+              @click="sortProductsByStatus"
               class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
               Unavailable
             </button>
@@ -210,6 +212,18 @@
                 console.log(error);
             });
     },
+    sortProductsByStatus(){
+                const sortFn = (a,b)=>{
+                    if(a.status=='available'){
+                        return 1
+                    }
+                    if(a.status!='available'){
+                        return -1
+                    }
+                    return 0
+                    }
+                this.order.sort(sortFn)
+            }
     },
     mounted() {
       this.getProducts();
