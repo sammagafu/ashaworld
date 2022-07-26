b51c997fb0bd916e6a3efcd079529369b8a95d5e
bichwaa <ptonny13@gmail.com>
2022-07-27 02:48:34 +0300

    Orders can be filtered

--- ashaadmin/src/views/Products.vue
+++ ashaadmin/src/views/Products.vue
@@ -70,10 +70,12 @@
               All Products
             </button>
             <button type="button"
+              @click="filterProductsByStatus('nothing')"
               class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
               available
             </button>
             <button type="button"
+              @click="filterProductsByStatus('available')"
               class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
               Unavailable
             </button>
@@ -360,6 +362,13 @@
               return 0
               }
           this.product.sort(sortFn)
+      },
+      filterProductsByStatus(status){
+        if(status!='nothing'){
+        this.product = this.product.filter(x=>x.status==status)
+        }else{
+          this.getProducts()
+        }
       }
     },
     mounted() {
