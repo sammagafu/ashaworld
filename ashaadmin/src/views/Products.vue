9322f8ff118febb6f6c91811e5b5e5869b4211c1
bichwaa <ptonny13@gmail.com>
2022-07-27 03:04:57 +0300

    Product filtering refactored

--- ashaadmin/src/views/Products.vue
+++ ashaadmin/src/views/Products.vue
@@ -66,6 +66,7 @@
         <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
           <div class="inline-flex rounded-md shadow-sm" role="group">
             <button type="button"
+              @click="filterProductsByStatus('nothing')"
               class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
               All Products
             </button>
