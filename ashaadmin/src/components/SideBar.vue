35b77524d2675731c750102b14723e4674e12435
bichwaa <ptonny13@gmail.com>
2022-07-28 07:21:29 +0300

    bubble in side nav to show number of pending orders

--- ashaadmin/src/components/SideBar.vue
+++ ashaadmin/src/components/SideBar.vue
@@ -39,10 +39,14 @@
                                         d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
                                 <span class="flex-1 ml-3 whitespace-nowrap">Orders</span>
+                                <span 
+                                :class="{ 'bg-red-600': pending, 'bg-gray-600':!pending }"
+                                class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 rounded-full"
+                                >{{pending}}</span>
                             </router-link>
                         </li>
 
-                        <!-- <li>
+                        <li>
                             <router-link :to="{name:'team'}"
                                 class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
@@ -52,7 +56,7 @@
                                 </svg>
                                 <span class="flex-1 ml-3 whitespace-nowrap">Team</span>
                             </router-link>
-                        </li> -->
+                        </li>
 
 
 
@@ -118,12 +122,37 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: "SideBar",
         data(){
             return {
-                isOpen : false,
+                orders:[],
+                pending:0
             }
+        },
+        methods:{
+            getOrders() {
+                axios.get('/order')
+                    .then(response => {
+                        this.orders = response.data;
+                    })
+                    .then(()=>this.updatePending())
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+            updatePending(){
+                this.orders.forEach(order=>{
+                if(order.orderstatus.toLowerCase()=="pending"){
+                    this.pending+=1;
+                }
+            })
+            }
+        },
+        mounted(){
+            this.getOrders();
         }
     }
 </script>
