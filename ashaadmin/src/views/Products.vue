cae76e3220da5c72a93f7e72dfba82755cb67c0a
bichwaa <ptonny13@gmail.com>
2022-07-28 08:06:46 +0300

    Products can be deleted from dashboard

--- ashaadmin/src/views/Products.vue
+++ ashaadmin/src/views/Products.vue
@@ -123,7 +123,7 @@
                     {{prod.id}}
                   </td>
                   <td class="px-6 py-4 whitespace-nowraw">
-                    <img :src="prod.get_coverImage" alt="">
+                    <img :src="prod.get_coverImage" alt="img">
                   </td>
                   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                     <span 
@@ -146,18 +146,22 @@
                   </td>
                   <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                     <div class="flex justify-between">
-                      <button type="button"
+                      <button 
+                        @click="openModal"
+                        type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor" stroke-width="2">
                           <path stroke-linecap="round" stroke-linejoin="round"
                             d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                         </svg>
                         Edit
                       </button>
-                      <button type="button"
+                      <button 
+                      @click="deleteProduct(prod.id)"
+                      type="button"
                         class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
                           stroke="currentColor" stroke-width="2">
                           <path stroke-linecap="round" stroke-linejoin="round"
                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
@@ -333,6 +337,16 @@
           }).catch(error => {
             console.log(error);
           });
+      },
+      deleteProduct(id){
+        axios.delete(`product/${id}`)
+            .then(response => {
+                this.product = this.product.filter(x=>{
+                if(x.id!=id) return x
+            });
+            }).catch(error => {
+                console.log(error);
+            });
       },
       exportAllProducts(){
           axios.get(`product/export_product_list`)
